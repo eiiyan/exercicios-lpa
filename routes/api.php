3f0b6bbdebb9378b9f3af52dd893fde64b9f36c5
eiiyangabriel1 <yanpandim77@gmail.com>
2024-03-08 10:23:04 -0300

    exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,8 @@ Route::get('receber/nome', function(Request $request){
 
 });
 
+Route::get('nome/idade' , function (Request $request){
+    $nome = $request->input('nome');
+    $idade = $request->input('idade');
+return 'Meu nome é ' . $nome . ', tenho ' .$idade . ' anos de vida ';
+});
