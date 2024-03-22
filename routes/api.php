e2801064232f4998f59f44205fe9c987508a7aef
eiiyangabriel1 <yanpandim77@gmail.com>
2024-03-22 15:41:13 -0300

    Exercicios 22/03

--- routes/api.php
+++ routes/api.php
@@ -139,9 +139,9 @@ Route::get('temperaturaatual', function (Request $request) {
     $temperatura = $request->input('temperatura');
 
     if ($temperatura > 30) {
-        return "Esta quente!";
+        return "Está quente!";
     } else {
-        return "Nao esta quente!";
+        return "Não está quente!";
     }
 });
 
@@ -179,4 +179,180 @@ Route::get('divisivel', function (Request $request) {
     }
 });
 
+Route::get('maioridade' , function (Request $request){
+$idade = $request->input('idade');
 
+if($idade >=18){
+    return "Maior de idade";
+} else {
+    return "Menor de idade";
+}
+});
+
+Route::get('verificar/par' , function (Request $request){
+$numero = $request->input('numero');
+
+if($numero % 2 == 0){
+    return "par";
+} else {
+    return "ímpar";
+}
+});
+
+Route::get('divisivel2' , function (Request $request){
+$numero = $request->input('numero');
+
+if($numero % 9 == 0){
+    return "Este número é divisível por 9";
+} else {
+    return "Este número não é divisível por 9";
+}
+});
+
+Route::get('múltiplo' , function (Request $request){
+$numero = $request->input('numero');
+
+if($numero % 7 == 0 ) {
+    return "Este número é múltiplo de 7";
+} else {
+    return "Este número não é múltiplo de 7";
+}
+});
+
+Route::get('crianca' , function (Request $request){
+$idade = $request->input('idade');
+
+if($idade < 12){
+    return "Você é uma criança";
+} else {
+    return "Você não é uma criança";
+}
+});
+
+Route::get('positivoimpar' , function (Request $request){
+$numero = $request->input('numero');
+
+if($numero % 2 == 0){
+    return "Positivo e Par";
+} else {
+    return "Positivo e Ímpar ";
+}
+});
+
+Route::get('maiorque100' , function (Request $request){
+$numero = $request->input('numero');
+
+if($numero >  100){
+    return "O " . $numero . " é maior que 100";
+} if($numero < 100){
+    return "O " . $numero . " é menor que 100";
+} else {
+    return "O " . $numero . " é igual a 100";
+}
+});
+
+Route::get('divisivel3' , function (Request $request){
+$numero = $request->input('numero');
+
+if($numero % 6 == 0){
+    return "Este número é divisível por 6";
+} else {
+    return "Este número não é divisível por 6";
+}
+});
+
+Route::get('alice' , function (Request $request){
+$nome = $request->input('nome');
+
+if($nome =="Alice"){
+    return "Olá, Alice";
+} else {
+    return "Olá, " . $nome . "Você não é Alice";
+}
+});
+
+
+Route::get('dirigir' , function (Request $request){
+$idade = $request ->input('idade');
+$carteira = $request->input('carteira');
+
+if($idade >=18){
+    return "Você pode dirigir, pois é maior de idade!";
+} else {
+return "Você não pode dirigir, pois é menor de idade";
+} if ($carteira ="Sim"){
+return "Você possui caretira de motorista";
+} else {
+return "Você não possui carteira de motorista";
+}
+});
+
+Route::get('menordeles' , function (Request $request){
+$numero1 = $request->input('numero1');
+$numero2 = $request->input('numero2');
+
+if($numero1 < $numero2){
+    return "O " . $numero1 . " é menor que o " . $numero2;
+} else {
+    return "O " . $numero2 . " é menor que o " . $numero1;
+}
+});
+
+Route::get('maioridade' , function (Request $request){
+$nome = $request->input('nome');
+$idade = $request->input('idade');
+
+if($idade >= 18){
+    return "Você é maior de idade, " . $nome;
+} else {
+    return "Você não é maior de idade, " . $nome;
+}
+if($nome =="Yan"){
+    return "Olá, Yan";
+} else {
+    return "Olá";
+} 
+});
+
+Route::get('divisao2' , function (Request $request){
+    $numero1 = $request->input('numero1');
+    $numero2 = $request->input('numero2');
+
+    if($numero1 == 0){
+        return "Não é possível efetuar a divisão pois o primeiro número é 0 ";
+    } else if ($numero2 == 0){
+        return "Não é possível efetuar a divisão pois o segundo número é 0 ";
+    } else {
+    return $numero1 / $numero2;
+    }
+});
+
+Route::get('multiplica100' , function (Request $request){
+$numero1 = $request->input('numero1');
+$numero2 = $request->input('numero2');
+
+if($numero1 * $numero2 >100){
+    return "Esta multiplicação é maior que 100";
+} else {
+    return "Esta multiplicação é menor que 100";
+} 
+});
+
+Route::get('soma2' , function (Request $request){
+    $numero1 = $request->input('numero1');
+    $numero2 = $request->input('numero2');
+     $soma = $numero1 + $numero2;
+     
+    if($soma % 2 == 0){
+return $numero1 * $numero2;
+    } else {
+        return $numero1 / $numero2;
+    }
+
+});
+
+
+
+
+
+    
